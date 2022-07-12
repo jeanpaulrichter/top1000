d7a0b8d9a556648166f9bb0d660d0bfcafb03eb0
jeanpaulrichter <webkonten.ls@hsslj.de>
2022-07-12 13:39:52 +0200

    added license

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -2,7 +2,7 @@
 This file is part of http://www.github.com/jeanpaulrichter/top1000
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
+as published by the Free Software Foundation; either version 3
 of the License, or (at your option) any later version.
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
